6c6cf47669dda430bd71f0ebc813893dfc9803f0
orange <jambonbill@gmail.com>
2020-11-21 12:11:09 +0100

    + * @version 1.0.1

--- src/PSP/Screen.php
+++ src/PSP/Screen.php
@@ -2,7 +2,7 @@
 /**
  * PSP Screen
  * Handle Screen memory page
- * @version 1.0.0
+ * @version 1.0.1
  * @author jambonbill
  */
 
@@ -17,8 +17,6 @@ class Screen
 
     private $isDebug=false;
 
-
-    //list of frames
     private $_chars=null;//CHAR PAGE
     private $_colrs=null;//COLR PAGE
 
@@ -26,18 +24,15 @@ class Screen
     private $_rows=0;//screen size
 
     private $_colors=["#000000","#ffffff","#ab3126","#66daff","#bb3fb8","#55ce58","#1d0e97","#eaf57c","#b97418","#785300","#dd9387","#5b5b5b","#8b8b8b","#b0f4ac","#aa9def","#b8b8b8"];//default 16 colors
+
     private $_charset=null;//pixel data?
     private $_bgColor=0;//black
+    private $_borderColor=0;//not implemented
 
-    /**
-     * Dependency injection
-     *
-     * @param Base $B [description]
-     */
+
     public function __construct(int $width, int $height)
     {
         $this->resize($width,$height);
-
         $charset=new Charset();
         $this->_charset=$charset->pixels();
     }
@@ -72,7 +67,7 @@ class Screen
      * @param  byte   $rows [description]
      * @return [type]       [description]
      */
-    public function resize(int $cols, int $rows)
+    public function resize(int $cols, int $rows): self
     {
         if($cols<0||$cols>255)throw new Exception("Screen resize Error 1", 1);
         if($rows<0||$rows>255)throw new Exception("Screen resize Error 2", 1);
@@ -85,17 +80,27 @@ class Screen
             $this->_chars[$i]=0;//`@`
             $this->_colrs[$i]=1;//black
         }
-        return true;
+        return $this;
     }
 
 
     /**
      * helper method
-     * @return [type] [description]
+     * @return [int] Total number of chars in Screen
      */
     public function charNumber(): int
     {
         return $this->_cols*$this->_rows;
+    }
+
+
+    /**
+     * Get/Set color palette
+     * @return [type] [description]
+     */
+    public function colorPalette()
+    {
+        return $this->_colors;
     }
 
 
@@ -113,8 +118,8 @@ class Screen
     }
 
 
-   public function fromDb(array $r)
-   {
+    public function fromDb(array $r)
+    {
         $jso=json_decode($r['json']);
         $colors=$jso->colors;
         //print_r($colors);exit;
@@ -128,7 +133,8 @@ class Screen
             $colr=ord($r['bin'][$i*2+1]);
             $this->poke($i,$char,$colr);
         }
-   }
+    }
+
 
     /**
      * Render screen to a PNG file and save at path
@@ -192,4 +198,40 @@ class Screen
         imagepng($im, $path);
     }
 
+
+    /**
+     * Build a PSP Json file
+     * @return [type] [description]
+     */
+    public function toJson(string $path): string
+    {
+        $arr=[];
+        $arr['version']=10;//json format version
+        $arr['cols']=$this->_cols;
+        $arr['rows']=$this->_rows;
+        $arr['bgColor']=$this->_bgColor;
+        $arr['colors']=$this->_colors;//palette
+
+        // this is ugly but i dont know how to do it right
+        // Splfixed array is not 8bit, and do not stringify well
+        $A='';
+        $B='';
+        for($i=0;$i<$this->charNumber();$i++){
+            $A.=chr($this->_chars[$i]);
+            $B.=chr($this->_colrs[$i]);
+        }
+
+        $arr['charData']=[];//Base64 encoded
+        $arr['charData'][0]=base64_encode($A);
+
+        $arr['colrData']=[];
+        $arr['colrData'][0]=base64_encode($B);
+        //var_dump($arr);exit;
+        $json=json_encode($arr);
+
+        if (($path)) {//save to file
+            file_put_contents($path, $json);
+        }
+        return $json;
+    }
 }
