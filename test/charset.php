671021340fd25c1ee2a5922aeaeb96f66a458b82
orange <jambonbill@gmail.com>
2020-11-22 13:35:37 +0100

    +$charset=new PSP\Charset();

---
+++ test/charset.php
@@ -0,0 +1,27 @@
+<?php
+//test charset (Load)
+require __DIR__."/../vendor/autoload.php";
+
+
+//$screen=new PSP\Screen(16,16);
+
+$charset=new PSP\Charset();
+
+//dump data
+//var_dump($charset->data());//exit;
+
+$charset->toPng("/tmp/charset-dump.png");
+//var_dump($charset->char(0));
+
+
+
+//dump all chars
+/*
+for($i=0;$i<$screen->charNumber();$i++){
+	$screen->poke($i,$i,$i);
+}
+
+$screen->toPng("/tmp/charset.png");
+*/
+
+exit("ok\n");
