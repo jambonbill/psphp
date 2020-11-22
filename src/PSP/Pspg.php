9b425c85ce072bea5dcb386d57fbef2fa399e853
orange <jambonbill@gmail.com>
2020-11-22 13:35:28 +0100

    private $_zoom=1;//Pixel size

--- src/PSP/Pspg.php
+++ src/PSP/Pspg.php
@@ -16,7 +16,7 @@ class Pspg
 {
 
     private $isDebug=false;
-
+    private $_zoom=1;//Pixel size
 
     public function __construct()
     {
@@ -24,13 +24,45 @@ class Pspg
         //$this->_charset=$charset->pixels();
     }
 
+    /**
+     * (from petscii.php) Decode a pspg frame string (B64+LZW)
+     * @param  string $str [description]
+     * @return [type]      [description]
+     */
+    /*
+    public function decodePspg($pstr='')
+    {
+        $b64str=$this->lzw_decode(trim($pstr));
+        $bstr=base64_decode($b64str);
+
+        $cols=ord($bstr[0]);
+        $rows=ord($bstr[1]);
+
+        $dat=[];
+        $dat['charset']='uppercase';
+        $dat['cols']=$cols;
+        $dat['rows']=$rows;
+
+        $chars=new \SplFixedArray($cols*$rows);
+        $colrs=new \SplFixedArray($cols*$rows);
+        for($i=0;$i<$cols*$rows;$i++){
+            $chars[$i]=ord($bstr[2+($i*2)]);
+            $colrs[$i]=ord($bstr[2+($i*2)+1]);
+        }
+        $dat['charData'][0]=$chars;
+        $dat['colrData'][0]=$colrs;
+        //print_r($dat);exit;
+        return $this->decode($dat);
+    }
+    */
+
 
     /**
      * Render frames to PNG at path
      * @param  string $path [description]
      * @return [type]       [description]
      */
-    public function render(string $path)
+    public function render(string $filename, string $path)
     {
         //
     }
